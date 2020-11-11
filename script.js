e719d532db7580eacee007036fbe464d57122241
Niza Vera <nizavera95@gmail.com>
2020-11-11 00:16:45 +0000

    ehancing generator

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = () => {
     //declare the variables who, what, when
-    let who = [ "My Cousin", "His Docter", "The Maniac", "The Tourist"];
+    let who = [ "My cousin", "A Docter", "The maniac", "A tourist"];
     //and assign at least 4 values for each
-    let what = [ "broke an arm", "won a million dollars", "broke a world record", "enjoys skydiving"];
-    let when = [ "last night", "over the weekend", "in your dreams", "during the pandemic"];
+    let what = [ "broke my arm", "won a million dollars", "slashed my tires", "ate my leftovers"];
+    let when = [ "last night", "in another dimension", "in your dreams", "during the pandemic"];
     //selecct a random who, what, and when
     var selectedWho = who[Math.floor(Math.random() * who.length)];
     var selectedWhat = what[Math.floor(Math.random() * what.length)];
@@ -11,5 +11,5 @@ window.onload = () => {
     //concatenate who + what + when 
     let excuse = `${selectedWho + " " + selectedWhat + " " + selectedWhen + "!"}`;
     //print webpage
-    document.getElementById("title").innerHTML = excuse;
+    document.getElementById("excuse").innerHTML = excuse;
 }
